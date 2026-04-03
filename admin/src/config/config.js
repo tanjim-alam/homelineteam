9cac57291fad75b9096a74729548376d75c17c1e
Nadeem <nadeem@example.com>
2026-04-03 11:27:42 +0530

    updated admin config

--- admin/src/config/config.js
+++ admin/src/config/config.js
@@ -2,7 +2,7 @@
 export const config = {
     // API Configuration
     // API_BASE_URL: import.meta.env.VITE_API_BASE_URL || 'https://api.homelineteam.com',
-    API_BASE_URL: 'http://localhost:5000',
+    API_BASE_URL: 'https://api.homelineteam.com',
     // App Configuration
     APP_NAME: import.meta.env.VITE_APP_NAME || 'HomeLine Admin',
 
