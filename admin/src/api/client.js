afe35aa08689fe391ed0da2770468c58fda0c136
Nadeem <nadeem@example.com>
2026-07-02 14:43:05 +0530

    updated api origin

--- admin/src/api/client.js
+++ admin/src/api/client.js
@@ -3,7 +3,7 @@ import config from '../config/config';
 
 // https://homelineteam-19e5.vercel.app
 const api = axios.create({
-  baseURL: config.API_BASE_URL || "http://localhost:5000",
+  baseURL: config.API_BASE_URL || "https://api.homelineteam.com",
   withCredentials: true,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
